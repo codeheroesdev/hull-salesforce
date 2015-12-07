47dfaf1875ea5119e2824104b605567354711976
Stephane Bellity <sbellity@gmail.com>
2015-12-07 14:55:51 +0100

    dist all assets

--- gulp_tasks/config.js
+++ gulp_tasks/config.js
@@ -4,9 +4,7 @@ module.exports = {
   outputFolder: outputFolder,
   files: {
     'manifest.json': outputFolder,
-    'assets/*.html': outputFolder,
-    'assets/*.png': outputFolder,
-    'assets/*.md': outputFolder
+    'assets/**/*': outputFolder
   }
 };
 
