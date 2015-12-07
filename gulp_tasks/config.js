05c8eca5a3b9ad02bab1af8c6a521bc0f39e4b1a
Stephane Bellity <sbellity@gmail.com>
2015-12-07 12:03:46 +0100

    gulp tooling

---
+++ gulp_tasks/config.js
@@ -0,0 +1,11 @@
+var outputFolder = 'dist';
+
+module.exports = {
+  outputFolder: outputFolder,
+  files: {
+    'manifest.json': outputFolder,
+    'assets/*png': outputFolder,
+    'assets/readme.md': outputFolder
+  }
+};
+
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,18 @@ var jest = require('jest-cli');
 var pkg = require('./package.json');
 var jestConfig = pkg.jest;
 
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+var config      = require('./gulp_tasks/config');
+
+[
+  'clean',
+  'cloudfront',
+  'copy',
+  'deploy',
+].map(function(task) { require('./gulp_tasks/' + task + '.js')(gulp, config);});
+
+
 gulp.task('test', function(done) {
     jest.runCLI({ config : jestConfig }, ".", function(res) {
       done();
@@ -14,6 +26,17 @@ gulp.task('tdd', function(done) {
     gulp.watch([ 'src/**/*.js' ], [ 'test' ]);
 });
 
+
+gulp.task('build', function(callback) {
+  runSequence('clean', ['copy'], callback);
+});
+
+gulp.task('deploy', function(callback) {
+  runSequence('build', 'gh:deploy', 'cloudfront', callback);
+});
+
 gulp.task('default', function() {
     // place code for your default task here
 });
+
+
