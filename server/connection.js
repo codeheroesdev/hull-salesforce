bc85e20a84c36cc026fc5ea4ef935216ee0aa860
Thomas Kgaevski <thomas@hull.io>
2017-03-06 11:56:15 +0100

    some coding style fixes

--- server/connection.js
+++ server/connection.js
@@ -1,13 +1,13 @@
-import jsforce from 'jsforce';
-import librato from 'librato-node';
+import jsforce from "jsforce";
+import librato from "librato-node";
 
 function increment(metric, value, options) {
   try {
     if (librato && librato.increment) {
       librato.increment(metric, value, options);
     }
-  } catch(err) {
-    // console.warn('Librato error', err)
+  } catch (err) {
+    // console.warn("Librato error", err)
   }
 }
 
@@ -16,8 +16,8 @@ function measure(metric, value, options) {
     if (librato && librato.measure) {
       librato.measure(metric, value, options);
     }
-  } catch(err) {
-    // console.warn('Librato error', err)
+  } catch (err) {
+    // console.warn("Librato error", err)
   }
 }
 
@@ -29,17 +29,16 @@ export default class Connection extends jsforce.Connection {
   }
 
   request(request, options, callback) {
-    increment('salesforce:requests', 1, { source: this._shipId });
+    increment("salesforce:requests", 1, { source: this._shipId });
     const ret = super.request(request, options, callback);
     ret.then((res) => {
       if (this.limitInfo && this.limitInfo.apiUsage) {
-        measure('salesforce:used', this.limitInfo.apiUsage.used, { source: this._shipId });
+        measure("salesforce:used", this.limitInfo.apiUsage.used, { source: this._shipId });
       }
     }, (res) => {
-      console.warn('salesforce API error', JSON.stringify({request, options, res}));
-      increment('salesforce:errors', 1, { source: this._shipId });
+      console.warn("salesforce API error", JSON.stringify({ request, options, res }));
+      increment("salesforce:errors", 1, { source: this._shipId });
     });
     return ret;
   }
-
 }
