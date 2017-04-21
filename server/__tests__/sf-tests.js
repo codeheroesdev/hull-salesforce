83de4e1611fc5c6c284c280ad1caf2fb728d9cf9
Michal Raczka <michal@raczka.me>
2017-04-21 10:53:21 +0200

    Fix tests

--- server/__tests__/sf-tests.js
+++ server/__tests__/sf-tests.js
@@ -13,10 +13,17 @@ const Mappings = {
   }
 };
 
+const hullStub = {
+  logger: {
+    info: () => {},
+    error: () => {}
+  }
+}
+
 describe('searchEmailsQuery', ()=> {
   it('build a query from a list of emails', ()=> {
     const SF = require('../sf').SF;
-    let sf = new SF();
+    let sf = new SF({}, hullStub);
     let emails = ['bob@bob.com', 'jon@jon.com'];
     let qry = sf.searchEmailsQuery(emails, Mappings);
     expect(qry).toEqual('FIND {"bob@bob.com" OR "jon@jon.com"} IN Email FIELDS RETURNING Foo(Id,Foo), Bar(Id,Bar)');
