cd54540443d01fdacd45d485b465d10e16a7ed36
mkawalek <michalkawalekdevkit@icloud.com>
2017-08-16 14:50:47 +0200

    status check finished

--- server/lib/status-check.js
+++ server/lib/status-check.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import Agent from "../agent";
-import { buildConfigFromShip } from '../config';
+import { buildConfigFromShip } from "../config";
 
 export default function statusCheck(req, res) {
   const { ship = {}, client = {}, shipApp = {} } = req.hull;
@@ -18,12 +18,48 @@ export default function statusCheck(req, res) {
     messages.push("External service credentials aren’t set: missing API refresh token.");
   }
 
+  if (_.isEmpty(_.get(ship, "private_settings.synchronized_segments", []))) {
+    status = "error";
+    messages.push("No segments will be synchronized because of missing configuration");
+  }
+
+  if (_.isEmpty(_.get(ship, "private_settings.leads_mapping", []))) {
+    status = "error";
+    messages.push("No leads will be sent from Hull to Salesforce due to missing configuration")
+  }
+
+  if (_.isEmpty(_.get(ship, "private_settings.fetch_lead_fields", []))) {
+    if (status !== "error") {
+      status = "warning";
+    }
+    messages.push("No lead fields will be fetched from Salesforce due to missing configuration")
+  }
+
+  if (_.isEmpty(_.get(ship, "private_settings.fetch_contact_fields", []))) {
+    if (status !== "error") {
+      status = "warning";
+    }
+    messages.push("No contacts fields will be fetched from Salesforce due to missing configuration")
+  }
+
+  if (_.isEmpty(_.get(ship, "private_settings.contacts_mapping", []))) {
+    if (status !== "error") {
+      status = "warning";
+    }
+    messages.push("No contacts will be sent from Hull to Salesforce due to missing configuration")
+  }
+
+  if (_.isEmpty(_.get(ship, "private_settings.salesforce_oauth_url"))) {
+    status = "error";
+    messages.push("oAuth url is empty");
+  }
+
   if (
     _.get(ship, "private_settings.login_url")
     && (!_.get(ship, "private_settings.salesforce_login") || !_.get(ship, "private_settings.salesforce_password"))
   ) {
     status = "error";
-    messages.push("External service credentials aren’t set: missing API login and password.");
+    messages.push("External service credentials aren’t set: missing API login and password");
   }
 
   if (messages.length === 0) {
@@ -31,7 +67,28 @@ export default function statusCheck(req, res) {
     const config = buildConfigFromShip(ship, organization, secret);
     const agent = new Agent(config);
 
-    promises.push(agent.connect().catch(err => {
+    promises.push(agent.connect().then(conn =>
+      Promise.all([
+        conn.describeGlobal((err, res) => {
+          if (err) {
+            status = "error";
+            return messages.push(`Error when trying to get test payload from SFDC API: ${_.get(err, "message", "Unknown")}`);
+          }
+          return res;
+        }),
+        conn.identity((err, res) => {
+          if (err) {
+            status = "error";
+            return messages.push(`Error when trying to get user identity from SFDC API: ${_.get(err, "message", "Unknown")}`);
+          }
+
+          if (_.isEmpty(res.user_id) || _.isEmpty(res.organization_id)) {
+            status = "error";
+            return messages.push("Got empty results from SFDC API")
+          }
+          return res;
+        })])
+    ).catch(err => {
       status = "error";
       messages.push(`Could not connect to SFDC API. Error: ${_.get(err, "message", "Unknown")}`);
     }));
